07867e771c145e500505234a7f2fd327f137b314
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-05-29 20:00:12 -0300

    ingresa una cadena de caracteres

--- Program.cs
+++ Program.cs
@@ -8,3 +8,12 @@ b = a ;
 
 Console.WriteLine("valor de a:"+a);
 Console.WriteLine("valor de b:"+b);
+
+
+//ejericio de invetir un numero 
+
+string numeroCaracter;
+
+Console.WriteLine("ingresar un numero");
+numeroCaracter = Console.ReadLine();
+
