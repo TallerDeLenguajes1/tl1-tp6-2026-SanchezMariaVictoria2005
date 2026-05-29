a6bf2b26af31f95a14a2f2f62a21df6e64ce11db
SanchezMariaVictoria2005 <mvictoria14092005@gmail.com>
2026-05-29 20:05:50 -0300

    varificacion de numero y convercion a entero

--- Program.cs
+++ Program.cs
@@ -17,3 +17,15 @@ string numeroCaracter;
 Console.WriteLine("ingresar un numero");
 numeroCaracter = Console.ReadLine();
 
+int numero;
+bool verificacion = int.TryParse(numeroCaracter, out numero);
+
+if (verificacion)
+{
+    Console.WriteLine("el caracter ingresado es un numero");
+    Console.WriteLine($"numero: {numero}");
+}
+else
+{
+    Console.WriteLine("la cadena ingresada no es un numero");
+}
